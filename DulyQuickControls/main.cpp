c57b2bbe73aa573d1d870a63d69f6695177edab8
Victor Gouet <victor.gouet@epitech.eu>
2017-09-18 11:33:20 -0400

    Test node with ANODE with Default Scene

--- DulyQuickControls/main.cpp
+++ DulyQuickControls/main.cpp
@@ -1,17 +1,32 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QSettings>
 #include <QDebug>
 
+#include "dulyscene.h"
+
+//#include <QGraphicsView>
+//#include <QGraphicsScene>
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
+    //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication app(argc, argv);
     app.setOrganizationName("Duly");
     app.setOrganizationDomain("Duly.com");
     app.setApplicationName("Duly Application");
 
+    qmlRegisterType<DulyScene>("DulyScene", 1, 0, "DulyScene");
+
+//    DulyScene dulyScene;
+
+
+   // (void)scene;
+
     QQmlApplicationEngine engine;
+
+//    QGraphicsScene scene;
+
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
