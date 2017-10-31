62684d27d32f01b7686202deb8e0e78f5dd31862
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2017-10-31 17:51:59 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
-pwd;
-ls;
-cd ./Duly-GUI/DulyQuickControls/;
-$CMAKE_PREFIX_PATH/../../bin/qmake DulyQuickControls.pro;
-ls
+cd ./Duly-GUI/Duly/;
+$CMAKE_PREFIX_PATH/../../bin/qmake Duly.pro;
+make;
+cd ./lib/EventClient
+$CMAKE_PREFIX_PATH/../../bin/qmake EventClient.pro;
+make;
+cd ../../Server;
+$CMAKE_PREFIX_PATH/../../bin/qmake Server.pro;
 make;
