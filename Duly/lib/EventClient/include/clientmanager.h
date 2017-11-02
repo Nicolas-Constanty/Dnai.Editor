1922f11075fc891f8855b87268e2a77a34d215c3
Victor Gouet <victor.gouet@epitech.eu>
2017-11-02 10:04:22 -0400

    description parser

--- Duly/lib/EventClient/include/clientmanager.h
+++ Duly/lib/EventClient/include/clientmanager.h
@@ -1,4 +1,21 @@
 #ifndef CLIENTMANAGER_H
 #define CLIENTMANAGER_H
 
+#include <QString>
+#include <QMap>
+#include "clientcommunication.h"
+
+class Q_DECL_EXPORT ClientManager {
+
+public:
+    static ClientManager &shared();
+
+public:
+    ClientCommunication *newClient(QHostAddress addr, quint16 port, QString const &name);
+    ClientCommunication *getFrom(QString const &name) const;
+
+private:
+    QMap<QString, ClientCommunication *> m_clients;
+};
+
 #endif // CLIENTMANAGER_H
--- Duly/lib/EventClient/include/descriptionparser.h
+++ Duly/lib/EventClient/include/descriptionparser.h
@@ -1,4 +1,36 @@
 #ifndef DESCRIPTIONPARSER_H
 #define DESCRIPTIONPARSER_H
 
+#include <QRegExp>
+#include <QString>
+#include <QList>
+
+class Q_DECL_EXPORT DescriptionParser {
+public:
+    typedef struct s_typeParser {
+        s_typeParser(char c, int size) : firstLetter(c), size(size) {
+
+        }
+
+        char firstLetter;
+        int size;
+    } TypeParser;
+
+public:
+    DescriptionParser();
+
+private:
+    void initReg();
+
+public:
+    void parse(QString const &);
+    QList<TypeParser> const &getList() const;
+
+private:
+    QList<QString> m_list;
+    QList<TypeParser> m_types;
+    QRegExp     m_rx;
+    QList<TypeParser> m_data;
+};
+
 #endif // DESCRIPTIONPARSER_H
