aeda5e3e9f70bacebdbd7816e534b13e75c12919
Quentin_Gasparotto <quentin.gasparotto@epitech.eu>
2018-03-17 19:49:48 -0400

    Removing data com event project
    Le core est dans le projet app pour ne pas créer de dépendances dans tous les sens

--- DNAI/lib/DataComEvent/Shared_include/commands/commands.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-
-#include "declarator/declare.h"
-
-#endif // COMMANDS_H
--- DNAI/lib/DataComEvent/Shared_include/replies/replies.h
+++
@@ -1,6 +0,0 @@
-#ifndef REPLIES_H
-#define REPLIES_H
-
-#include "declared.h"
-
-#endif // REPLIES_H
