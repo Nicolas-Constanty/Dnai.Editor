d329b178b0d38b1bc930cad2cea773033abc389a
Quentin_Gasparotto <quentin.gasparotto@epitech.eu>
2018-05-27 17:44:18 +0200

    Add enum callbacks for in handler

--- Gui/app/include/dnai/core/enumhandler.h
+++ Gui/app/include/dnai/core/enumhandler.h
@@ -24,12 +24,12 @@ namespace dnai
             void onEntityAdded(::core::EntityID id, models::Entity &entity);
 
         public:
-            Q_INVOKABLE void setValue();
-            Q_INVOKABLE void removeValue();
+            Q_INVOKABLE void setValue(::core::EntityID entity, QString const &name, QJsonValue const &value);
+            Q_INVOKABLE void removeValue(::core::EntityID entity, QString const &name);
 
         private:
-            void onValueSet(::core::EntityID enumeration, QString const &name);
-            void onSetValueError(::core::EntityID enumeration, QString const &name, QString const &message);
+            void onValueSet(::core::EntityID enumeration, QString const &name, QString const &value);
+            void onSetValueError(::core::EntityID enumeration, QString const &name, QString const &value, QString const &message);
 
             void onValueRemoved(::core::EntityID enumeration, QString const &name);
             void onRemoveValueError(::core::EntityID enumeration, QString const &name, QString const &message);
--- Gui/app/src/dnai/core/enumhandler.cpp
+++ Gui/app/src/dnai/core/enumhandler.cpp
@@ -1,9 +1,17 @@
 #include <functional>
 
+#include <QJsonDocument>
+#include <QJsonValue>
+#include <QStringList>
+
 #include "dnai/core/enumhandler.h"
 
 #include "dnai/commands/commandmanager.h"
 #include "dnai/commands/corecommand.h"
+
+#include "dnai/models/gui/declarable/enumtype.h"
+
+#include "dnai/editor.h"
 
 using namespace std::placeholders;
 
@@ -22,65 +30,74 @@ namespace dnai
             QObject::connect(&manager,  SIGNAL(entityAdded(enums::core::EntityID,models::Entity&)),
                              this,      SLOT(onEntityAdded(enums::core::EntityID,models::Entity&)));
 
-            ::core::enumeration::onValueSet(std::bind(&EnumHandler::onValueSet, this, _1, _2));
-            ::core::enumeration::onSetValueError(std::bind(&EnumHandler::onSetValueError, this, _1, _2, _3));
+            ::core::enumeration::onValueSet(std::bind(&EnumHandler::onValueSet, this, _1, _2, _3));
+            ::core::enumeration::onSetValueError(std::bind(&EnumHandler::onSetValueError, this, _1, _2, _3, _4));
             ::core::enumeration::onValueRemoved(std::bind(&EnumHandler::onValueRemoved, this, _1, _2));
             ::core::enumeration::onRemoveValueError(std::bind(&EnumHandler::onRemoveValueError, this, _1, _2, _3));
         }
 
         void EnumHandler::onEntityAdded(::core::EntityID id, models::Entity &entity)
         {
+            models::gui::declarable::EnumType *enumtype = entity.guiModel<models::gui::declarable::EnumType>();
 
+            //if entity is an enum
+            if (enumtype != nullptr)
+            {
+                int val = 0;
+                //replicate its values on core
+                for (auto nam : enumtype->values())
+                {
+                    setValue(id, nam, val);
+                }
+            }
         }
 
-        void EnumHandler::setValue()
+        void EnumHandler::setValue(::core::EntityID entity, QString const &name, QJsonValue const &value)
         {
-            /*
             commands::CommandManager::Instance()->exec(
                 new commands::CoreCommand("Enum.SetValue", true,
-                    std::bind(::core::enumeration::setValue(...)),
-                    std::bind(::core::enumeration::removeValue(...))
+                    std::bind(&::core::enumeration::setValue, entity, name, value.toString()),
+                    std::bind(&::core::enumeration::removeValue, entity, name)
                 )
             );
-            */
         }
 
-        void EnumHandler::removeValue()
+        void EnumHandler::removeValue(::core::EntityID entity, QString const &name)
         {
-            /*
+            const int value = manager.getEntity(entity).guiModel<models::gui::declarable::EnumType>()->values().indexOf(name);
+
             commands::CommandManager::Instance()->exec(
                 new commands::CoreCommand("Enum.SetValue", true,
-                    std::bind(::core::enumeration::removeValue(...)),
-                    std::bind(::core::enumeration::setValue(...))
+                    std::bind(&::core::enumeration::removeValue, entity, name),
+                    std::bind(&::core::enumeration::setValue, entity, name, QString::number(value))
                 )
             );
-            */
         }
 
-        void EnumHandler::onValueSet(::core::EntityID enumeration, const QString &name)
+        void EnumHandler::onValueSet(::core::EntityID enumeration, const QString &name, QString const &)
         {
-            Q_UNUSED(enumeration)
-            Q_UNUSED(name)
+            //QJsonValue val = QJsonDocument::fromJson(value.toUtf8()).toVariant().toJsonValue();
+
+            manager.getEntity(enumeration).guiModel<models::gui::declarable::EnumType>()->addEntry(name);// setValue(name, val);
+            commands::CoreCommand::Success();
         }
 
-        void EnumHandler::onSetValueError(::core::EntityID enumeration, const QString &name, const QString &message)
+        void EnumHandler::onSetValueError(::core::EntityID, const QString &name, QString const &value, const QString &message)
         {
-            Q_UNUSED(enumeration)
-            Q_UNUSED(name)
-            Q_UNUSED(message)
+            commands::CoreCommand::Error();
+            dnai::Editor::instance().notifyError("Cannot set \"" + name + "\" to value \"" + value + "\" to enumeration: " + message, [](){});
         }
 
         void EnumHandler::onValueRemoved(::core::EntityID enumeration, const QString &name)
         {
-            Q_UNUSED(enumeration)
-            Q_UNUSED(name)
+            manager.getEntity(enumeration).guiModel<models::gui::declarable::EnumType>()->deleteEntry(name);
+            commands::CoreCommand::Success();
         }
 
-        void EnumHandler::onRemoveValueError(::core::EntityID enumeration, const QString &name, const QString &message)
+        void EnumHandler::onRemoveValueError(::core::EntityID, const QString &name, const QString &message)
         {
-            Q_UNUSED(enumeration)
-            Q_UNUSED(name)
-            Q_UNUSED(message)
+            commands::CoreCommand::Error();
+            dnai::Editor::instance().notifyError("Cannot remove value \"" + name + "\" from enumeration: " + message, [](){});
         }
     }
 }
