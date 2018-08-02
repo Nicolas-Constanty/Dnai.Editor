d3a7734b97ecdf6bc76b8e4527aa60b73fbec52e
Nicolas Constanty <nicolas.constanty@epitech.eu>
2018-08-02 07:47:28 +0200

    0.2.0

--- scripts/windows/install_editor.cmd
+++ scripts/windows/install_editor.cmd
@@ -1,3 +1,6 @@
+echo y | DEL .\app\release\%INSTALL_FOLDER%\.moc /Q
+echo y | DEL .\app\release\%INSTALL_FOLDER%\.obj /Q
+echo y | DEL .\app\release\%INSTALL_FOLDER%\.rcc /Q
 windeployqt.exe "./app/release/DNAI.exe" -qmldir=%APPVEYOR_BUILD_FOLDER%\Gui\app\resources -verbose=2
 dir /b "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC" | tail.exe -1 > mredist.txt
 set /p MREDIST=<mredist.txt
