5820f6e4f0271edba240d90ebb6408e74f0b578f
Nicolas Constanty <nicolas.constanty@epitech.eu>
2018-08-02 04:41:13 +0200

    update appveyor

--- scripts/windows/install_jq.ps1
+++ scripts/windows/install_jq.ps1
@@ -1,6 +1,3 @@
 if($env:APPVEYOR_REPO_TAG -eq 'true') {
-    mkdir %APPVEYOR_BUILD_FOLDER%\Downloads
-    cd %APPVEYOR_BUILD_FOLDER%\Downloads
     Start-FileDownload https://github.com/stedolan/jq/releases/download/jq-1.5/jq-win64.exe
-    set PATH=%APPVEYOR_BUILD_FOLDER%\Downloads;%PATH%
 }
