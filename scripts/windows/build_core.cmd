f5efe0bcbdc27e759338887c3ee37ab72177c501
Nicolas Constanty <nicolas.constanty@epitech.eu>
2018-12-05 18:30:12 +0100

    Restore build_core.cmd

--- scripts/windows/build_core.cmd
+++ scripts/windows/build_core.cmd
@@ -1,4 +1,4 @@
 cd %APPVEYOR_BUILD_FOLDER%\Duly
 mkdir %APPVEYOR_BUILD_FOLDER%\Core
-dotnet restore
+nuget restore CorePackage.sln
 MSBuild.exe %APPVEYOR_BUILD_FOLDER%\Duly\CoreDaemon\CoreDaemon.csproj  /t:Rebuild /p:Configuration=Release;Platform=x64;OutputPath=%APPVEYOR_BUILD_FOLDER%\Core
