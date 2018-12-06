40b027dc3f6e0d8195060982ce865de036645cee
Nicolas Constanty <nicolas.constanty@epitech.eu>
2018-12-06 06:28:35 +0100

    Try copy quazip

--- scripts/windows/install_editor.cmd
+++ scripts/windows/install_editor.cmd
@@ -1,7 +1,7 @@
 echo y | DEL .\app\release\%INSTALL_FOLDER%\.moc /Q
 echo y | DEL .\app\release\%INSTALL_FOLDER%\.obj /Q
 echo y | DEL .\app\release\%INSTALL_FOLDER%\.rcc /Q
-copy "./lib/quazip.dll" "./app/"
+copy "%APPVEYOR_BUILD_FOLDER%\Gui\lib\quazip.dll" ".\app\"
 windeployqt.exe "./app/release/DNAI.exe" -qmldir=%APPVEYOR_BUILD_FOLDER%\Gui\app\resources -verbose=2
 dir /b "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC" | tail.exe -1 > mredist.txt
 set /p MREDIST=<mredist.txt
